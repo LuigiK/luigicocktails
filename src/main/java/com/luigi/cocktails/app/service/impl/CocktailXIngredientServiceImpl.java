2c4711ed48d7de978b94d04df7e66702cd8913bd
Luigi <ludovit.luigi.kovac@gmail.com>
2019-11-09 07:52:47 +0100

    CocktailXIngredientService

---
+++ src/main/java/com/luigi/cocktails/app/service/impl/CocktailXIngredientServiceImpl.java
@@ -0,0 +1,25 @@
+package com.luigi.cocktails.app.service.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.luigi.cocktails.app.model.TCocktailXIngredient;
+import com.luigi.cocktails.app.repository.ICocktailXIngredientRepository;
+import com.luigi.cocktails.app.service.ICocktailXIngredientService;
+
+public class CocktailXIngredientServiceImpl implements ICocktailXIngredientService {
+
+	@Autowired
+	ICocktailXIngredientRepository cocktailXIngredientRepository;
+	
+	@Override
+	public List<TCocktailXIngredient> findAllCocktailsByIngredientIds(List<Long> ingredientIds) {
+
+		return cocktailXIngredientRepository.findAll().stream()
+				.filter(record -> ingredientIds.contains(record.getIngredient().getIngredientId())).distinct()
+				.collect(Collectors.toList());
+	}
+
+}
